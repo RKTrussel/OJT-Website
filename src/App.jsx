121337136f943d3a9c297150305e19b7f7fb3ca5
tortangChris <eliseriochrisashley12@gmail.com>
2026-01-15 09:18:13 +0800

    Add App component and integrate into main rendering

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import CliberducheWebsite from "./pages/CliberducheWebsite";
+
+function App() {
+  return <CliberducheWebsite />;
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css';
-import MainPage from './MainPage.jsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+// import MainPage from './MainPage.jsx';
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <MainPage />
-  </StrictMode>,
-)
+    {/* <MainPage /> */}
+    <App />
+  </StrictMode>
+);
