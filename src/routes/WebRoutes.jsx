a2e570ae30ebc285b970e2a44a87a588e82582c0
tortangChris <eliseriochrisashley12@gmail.com>
2026-01-18 23:47:53 +0800

    Refactor WebRoutes for navigation

--- src/routes/WebRoutes.jsx
+++ src/routes/WebRoutes.jsx
@@ -4,21 +4,22 @@ import { Navigate } from "react-router-dom";
 const CliberducheWebsite = React.lazy(() =>
   import("../pages/CliberducheWebsite")
 );
+const SampleProjects = React.lazy(() => import("../projects/SampleProjects"));
 
 const WebRoutes = {
   path: "/",
   children: [
     {
       index: true,
-      element: <Navigate to="cliberduche-website/" replace />,
+      element: <Navigate to="cliberduche-website" replace />,
     },
     {
       path: "cliberduche-website",
-      element: <Navigate to="/cliberduche-website/" replace />,
+      element: <CliberducheWebsite />,
     },
     {
-      path: "cliberduche-website/",
-      element: <CliberducheWebsite />,
+      path: "cliberduche-website/sample-project",
+      element: <SampleProjects />,
     },
   ],
 };
